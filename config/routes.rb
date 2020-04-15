9477fd4da62bafc8b041055556abacd4a5a70998
okapyy <m9_17g_suki@yahoo.co.jp>
2020-04-15 13:44:09 +0900

    ユーザーと動画投稿まではできた

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'videos/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # devise_for :users
 
   root to: 'videos#index'
 
   resources :videos
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
